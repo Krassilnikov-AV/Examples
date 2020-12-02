a3366b55043b8f8b337b028e321f13d4f2a96539
Krassilnikov-AV <kavkras83@gmail.com>
2020-12-02 21:40:41 +0300

    рефакторинг, реализация со стримами

--- ExampleCompareComparator/src/example/Main.java
+++ ExampleCompareComparator/src/example/Main.java
@@ -1,52 +1,46 @@
 package example;
 
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
 
-    private static ArrayList<Car> cars = new ArrayList();
+	private static List<Car> cars = new ArrayList();
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Car car1 = new Car("BMV", 1961);
-        Car car2 = new Car("Lambordgini", 1945);
-        Car car3 = new Car("Opel", 2013);
-        Car car4 = new Car("Nissan", 1990);
-        Car car5 = new Car("Mercedes", 1985);
-        Car car6 = new Car("Volga", 1999);
-        Car car7 = new Car("Ford", 2015);
-        Car car8 = new Car("Toyota", 1981);
-
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-        cars.add(car4);
-        cars.add(car5);
-        cars.add(car6);
-        cars.add(car7);
-        cars.add(car8);
+		cars = Stream.of(new Car("BMV", 1961),
+			new Car("Lambordgini", 1945),
+			new Car("Opel", 2013),
+			new Car("Nissan", 1990),
+			new Car("Mercedes", 1985),
+			new Car("Volga", 1999),
+			new Car("Ford", 2015),
+			new Car("Toyota", 1981)).collect(Collectors.toList());
 
 //        Collections.sort(cars);
-        Collections.sort(cars, new CarYearComparator());
-        print();
+		//Collections.sort(cars, new CarYearComparator());
+		print();
+		cars.stream().sorted( new CarYearComparator()).forEach(car->{
+			System.out.println(car.getName() + " " + car.getYear());
+		});
+//		System.out.println("Самое старое авто: " + cars.get(0).getName());
+//		int anzahl = cars.size();
+//		int nou = anzahl - 1;
+//		System.out.println("Самое новое авто: " + cars.get(nou).getName());
+//		System.out.println("численость автопарка: " + anzahl);
 
-        System.out.println("Самое старое авто: " + cars.get(0).getName());
-int anzahl = cars.size();
-int nou = anzahl-1;
-        System.out.println("Самое новое авто: "+ cars.get(nou).getName());
-        System.out.println("численость автопарка: " + anzahl);
 
-        
+	}
+
+	public static void print() {
+		System.out.println();
+		Iterator<Car> iterator = cars.iterator();
+
+		while (iterator.hasNext()) {
+			Car car = iterator.next();
+			System.out.println(car.getName() + ", " + car.getYear());
+		}
+
+	}
 }
-
-    public static void print() {
-        System.out.println();
-        Iterator<Car> iterator = cars.iterator();
-
-        while(iterator.hasNext()) {
-            Car car = iterator.next();
-            System.out.println(car.getName()+", "+ car.getYear());
-        }
-
-        }
-    }
